b9d5cb13b8990521f7e1677995c9eca1d5c7c2c6
Onur BAYDAŞ <41346713+onurbaydas1@users.noreply.github.com>
2022-09-04 23:34:39 +0300

    Add files via upload

---
+++ insertionsort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void display(int arr[], int n) {     
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+
+void arayasokma(int D[], int N){
+    
+    int i, k, ekle;
+    for (i=1;i<N;i++) {
+        ekle=D[i];
+        for(k=i-1; k>=0 && ekle<=D[k] ; k--){
+            D[k+1]=D[k];
+        }
+        D[k+1]=ekle;
+        
+    }
+    
+    
+}
+int main()
+{
+    int D[7]={7,6,4353,231,3,9,10};
+    int N = 7;
+    display(D,N);
+    arayasokma(D,N);
+    display(D,N);
+    
+}
